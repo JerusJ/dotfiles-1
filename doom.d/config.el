0197c09d04f2ec48b9f57a6becf57792b7c705fe
Jesse Rusak <rusak.jesse@gmail.com>
2021-04-03 12:10:48 -0400

    fix: redundant packages

--- doom.d/config.el
+++ doom.d/config.el
@@ -4,23 +4,10 @@
 ;; sync' after modifying this file!
 ;;
 ;; See: https://github.com/syl20bnr/spacemacs/issues/6470 for how to set ITERM
-
-
-;; Disable nagging exit prompt
-(setq confirm-kill-emacs nil)
-
-;; Syntax Highlighting
-(add-to-list 'auto-mode-alist '("\\Jenkinsfile\'" . groovy-mode))
-
-;; Mac Rebindings
-(setq mac-option-modifier 'super)
-(setq mac-command-modifier 'meta)
-
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
 (setq user-full-name "Jesse Rusak"
       user-mail-address "rusak.jesse@gmail.com")
-
 ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
 ;; are the three important ones:
 ;;
@@ -31,8 +18,8 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "Source Code Pro" :size 32 :weight 'semibold)
-      doom-variable-pitch-font (font-spec :family "Source Code Pro" :size 32))
+(setq doom-font (font-spec :family "Source Code Pro" :size 16 :weight 'semibold)
+      doom-variable-pitch-font (font-spec :family "Source Code Pro" :size 14))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
@@ -63,3 +50,38 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
+;; Org Mode
+; (use-package org-bullets
+;   :after org
+;   :hook
+;   (org-mode . (lambda () (org-bullets-mode 1))))
+(use-package ox-html
+  :ensure nil
+  :defer 3
+  :after org
+  :custom
+  (org-html-checkbox-type 'unicode))
+(use-package ox-md
+  :ensure nil
+  :defer 3
+  :after org)
+(use-package ox-jira
+  :defer 3
+  :after org)
+;;(use-package org-jira
+;;  :defer 3
+;;  :after org
+;;  :custom
+;;  (jiralib-url "https://jira.work.com"))
+(use-package ox-confluence
+  :defer 3
+  :ensure nil
+  :after org)
+
+;; Syntax Highlighting
+(add-to-list 'auto-mode-alist '("\\Jenkinsfile\'" . groovy-mode))
+
+;; Mac Rebindings
+(setq mac-option-modifier 'super)
+(setq mac-command-modifier 'meta)
--- doom.d/init.el
+++ doom.d/init.el
@@ -30,7 +30,7 @@
        doom              ; what makes DOOM look the way it does
        doom-dashboard    ; a nifty splash screen for Emacs
        doom-quit         ; DOOM quit-message prompts when you quit Emacs
-       ;;(emoji +unicode)  ; 🙂
+       (emoji +unicode)  ; 🙂
        ;;fill-column       ; a `fill-column' indicator
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        ;;hydra
@@ -66,9 +66,9 @@
        ;;word-wrap         ; soft wrapping with language-aware indent
 
        :emacs
-       dired             ; making dired pretty [functional]
+       (dired +icons)             ; making dired pretty [functional]
        electric          ; smarter, keyword-based electric-indent
-       ;;ibuffer         ; interactive buffer management
+       ibuffer         ; interactive buffer management
        undo              ; persistent, smarter undo for your inevitable mistakes
        vc                ; version-control and Emacs, sitting in a tree
 
@@ -80,8 +80,8 @@
 
        :checkers
        syntax              ; tasing you for every semicolon you forget
-       (spell +flyspell) ; tasing you for misspelling mispelling
-       grammar           ; tasing grammar mistake every you make
+       ;;(spell +flyspell) ; tasing you for misspelling mispelling
+       ;;grammar           ; tasing grammar mistake every you make
 
        :tools
        ansible
@@ -137,7 +137,7 @@
        ;;javascript        ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
-       ;;latex             ; writing papers in Emacs has never been so fun
+       latex             ; writing papers in Emacs has never been so fun
        ;;lean
        ;;factor
        ;;ledger            ; an accounting system in Emacs
