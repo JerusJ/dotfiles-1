f09ebb03ffa6f8c2a4882f91d05d3abd1f0328e6
Jesse Rusak <rusak.jesse@gmail.com>
2021-04-16 14:28:10 -0400

    fix(emacs): conditional font setting based on platform

--- doom.d/config.el
+++ doom.d/config.el
@@ -3,37 +3,25 @@
 ;; Place your private configuration here! Remember, you do not need to run 'doom
 ;; sync' after modifying this file!
 ;;
-;; See: https://github.com/syl20bnr/spacemacs/issues/6470 for how to set ITERM
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets.
+;;
 (setq user-full-name "Jesse Rusak"
       user-mail-address "rusak.jesse@gmail.com")
-;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
-;; are the three important ones:
-;;
-;; + `doom-font'
-;; + `doom-variable-pitch-font'
-;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;;
-;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
-;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "Iosevka Term" :size 17 :weight 'semibold)
-      doom-variable-pitch-font (font-spec :family "Iosevka Term" :size 17))
 
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
+;; Fonts
+;; TODO: if/else
+(cond
+  ((string-equal system-type "gnu/linux")
+        (setq doom-font (font-spec :family "Iosevka Term" :size 36 :weight 'semibold)
+                doom-variable-pitch-font (font-spec :family "Iosevka Term" :size 36))))
+(cond
+  ((string-equal system-type "darwin")
+        (setq doom-font (font-spec :family "Iosevka Term" :size 17 :weight 'semibold)
+                doom-variable-pitch-font (font-spec :family "Iosevka Term" :size 17))))
+
 (setq doom-theme 'doom-one)
 
-;; Org Settings
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
-;; (add-to-list 'org-structure-template-alist '("r" "#+begin_src rust :exports both \nfn main() {\n?\n}\n#+end_src"))
 
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type t)
 
 ;; Here are some additional functions/macros that could help you configure Doom:
